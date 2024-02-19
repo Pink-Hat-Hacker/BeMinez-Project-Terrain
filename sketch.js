34279f4df1d5be290e625c392cdcfbfb7a6c445a
Pink-Hat-Hacker <zyv@udel.edu>
2024-02-18 21:27:32 -0500

    customize color, speed, and height

--- sketch.js
+++ sketch.js
@@ -1,107 +1,138 @@
+/**
+ * Perlin Noise Hover Terrain
+ * Uses noise(), beginShape(), and a matrix
+ *
+ * Z-Axis is changed via the songs amplitude
+ * Uses p5.Amplitude() and getLevel()
+ *
+ * The Coding Train Challenge #11 inspo
+ * By - Zoë Valladares aka Pink Hat Hacker
+ */
+
 var song;
 
-function preload(){
-    song = loadSound('be-minez.mp3');
-}
-
-/*function setup(){
-    createCanvas(800,600);
-    background(0);
-    
-    amplitude = new p5.Amplitude();
-    song.play();
-    amplitude.setInput(song);
-}
-
-function draw(){
-    background(0);
-    //fill(255);
-    //noStroke();
-    var level= amplitude.getLevel();
-    var size = map(level,0,.5,0,200);
-    
-    for(var rows = 0; rows<70; rows++ ){
-    for(var columns = 0; columns < 100; columns++){
-      fill(random(50,255), random(40,100), random(180,255));
-      ellipse(columns*20, rows*20, size, size);  
-    }
-  }
-    
-    //ellipse(width/2, height/2, size, size);
-}*/
-
-
-
 var cols, rows;
-var scl = 20;
+var scl = 15;
 var w = 1400;
 var h = 1000;
-
 var hover = 0;
-
 var terrain = [];
 
+let backgroundColorPicker;
+
+function preload() {
+  song = loadSound("be-minez.mp3");
+}
 
 function setup() {
   createCanvas(600, 600, WEBGL);
-
-  //frameRate(24);
-  
+  song.play();
   amplitude = new p5.Amplitude();
   amplitude.setInput(song);
-  song.play();
-  
+
+  /**
+   * Customize the Canvas and Terrain:
+   *
+   * canvas background color picker
+   * random leaning terrain rgb color sliders
+   * hover speed slider
+   * z-axis height - peak and trough inputs
+   */
+
+  // BackgroundColorPicker Module and Text
+  backgroundColorPicker = createColorPicker("black");
+  backgroundColorPicker.position(10, 640);
+  bcpText = createP("Background Color Picker");
+  bcpText.position(10, 600);
+
+  // Terrain RGB Sliders and Text
+  redSlider = createSlider(0, 255, 0);
+  redSlider.position(200, 640);
+  greenSlider = createSlider(0, 255, 0);
+  greenSlider.position(200, 660);
+  blueSlider = createSlider(0, 255, 0);
+  blueSlider.position(200, 680);
+  tcpText = createP("Random RGB Terrain Color Picker (kinda)");
+  tcpText.position(200, 600);
+
+  // Hover Speed Slider and Text
+  hoverSlider = createSlider(0, 3, 0.05, 0.01);
+  hoverSlider.position(10, 740);
+  hsText = createP("Hover Speed Slider");
+  hsText.position(10, 700);
+
+  // Z-axis Highest Peak and Lowest Trough inputs
+  pAndtInput = createInput(50);
+  pAndtInput.position(200, 740);
+  pAtText = createP("Highest Peak and Lowest Trough Value");
+  pAtText.position(200, 700);
+
   cols = w / scl;
-  rows = h/ scl;
+  rows = h / scl;
 
   for (var x = 0; x < cols; x++) {
     terrain[x] = [];
     for (var y = 0; y < rows; y++) {
-      terrain[x][y] = 0; //specify a default value 
+      terrain[x][y] = 0; //specify a default value
     }
   }
 }
 
 function draw() {
-    var level= amplitude.getLevel();
-    //var size = map(level,0,.5,0,200);
-    
-    // using the flying effect, combining Noise and Amplitude 
-  hover -= 0.05;
+  background(backgroundColorPicker.color());
+  
+  /**
+   * Moving the terrain based on Hover Speed and Music Amplitude
+   * 
+   * get amplitude level
+   * get and change hover speed value from slider (flying effect)
+   * smooth the terrain using perlin noise()
+   * +/- peak and trough height from input value
+   */
+  var level = amplitude.getLevel();
+  hover -= hoverSlider.value();
   var yoff = hover;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(level,noise(xoff, yoff), 0, 10, -150, 100);
+      // terrain[x][y] = map(level, noise(xoff, yoff), 0, 10, -150, 100);
+      terrain[x][y] = map(
+        level,
+        0,
+        noise(xoff, yoff),
+        -pAndtInput.value(),
+        abs(pAndtInput.value())
+      );
       xoff += 0.2;
     }
     yoff += 0.2;
   }
 
+  /**
+   * Creating terrain
+   * 
+   * line stroke color
+   * rotate X-Axis to make grid look 3D
+   * move grid to center
+   */
+  stroke("gray");
+  rotateX(PI / 2.5);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    // filling terrain color using rgb sliders
+    fill(
+      random(redSlider.value() - 50, redSlider.value() + 50),
+      random(greenSlider.value() - 50, greenSlider.value() + 50),
+      random(blueSlider.value() - 50, blueSlider.value() + 50),
+      100
+    );
 
-  
-  background(0);
-  
-  //stroke(0);
-  stroke(174, 186, 191);
-  
-  translate(0, 50);
-  rotateX(PI/2.5);
-  //fill(random(50,200),random(40,250),random(200,255), 100);
-  translate(-w/2, -h/2);
-  
-  //color, angle, plain development 
-  for (var y = 0; y < rows-1; y++) {
-    //fill(random(60,180),random(50,255),random(130,255), 100);
-    fill(random(60,180), random(160,255), 255, 100);
+    // terrain grid vertex shape
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
-      vertex(x*scl, y*scl, terrain[x][y]);
-      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
     endShape();
   }
-  
- 
 }
-
